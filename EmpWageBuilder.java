5ed05cfa478292f98b23beb3258f86e25cbdca25
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-09-23 01:19:27 +0530

    project completed

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,16 +1,32 @@
 public class EmpWageBuilder{
+
+		public static final int NO_OF_WORKING_DAYS = 20;
+	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int IS_FULL_TIME = 2;
+	public static final int IS_PART_TIME = 1;
+	public static final int MAX_WORKING_HOURS = 100;
+
 	public static void main(String[] argc) {
-		final int IS_PART_TIME = 1;
-		final int IS_FULL_TIME = 2;
-		final int EMP_RATE_PER_HOUR = 20;
 		int empHrs = 0;
 		int empWage = 0;
-		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if (empCheck == IS_FULL_TIME)
-			empHrs = 8;
-		else if (empCheck == IS_PART_TIME)
-			empHrs = 4;
-		empWage = empHrs * EMP_RATE_PER_HOUR;
+		int totalWorkingHrs = 0;
+		int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+		for (int day = 0; day < NO_OF_WORKING_DAYS && totalWorkingHrs < MAX_WORKING_HOURS; day++) {
+			switch (empCheck) {
+				case IS_FULL_TIME:
+					empHrs = 8;
+					break;
+				case IS_PART_TIME:
+					empHrs = 4;
+					break;
+				default:
+					empHrs = 0;
+			}
+
+			totalWorkingHrs += empHrs;
+		}
+
+		empWage = totalWorkingHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Emp Wage: " + empWage);
 	}
 }
