d0b7241605f975e913896b98982000158bad43c0
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-09-23 01:04:38 +0530

    1 Day part time Wage calculate

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,12 +1,15 @@
 public class EmpWageBuilder{
 	public static void main(String[] argc) {
-		final int IS_FULL_TIME = 1;
+		final int IS_PART_TIME = 1;
+		final int IS_FULL_TIME = 2;
 		final int EMP_RATE_PER_HOUR = 20;
 		int empHrs = 0;
 		int empWage = 0;
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		if (empCheck == IS_FULL_TIME)
 			empHrs = 8;
+		else if (empCheck == IS_PART_TIME)
+			empHrs = 4;
 		empWage = empHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Emp Wage: " + empWage);
 	}
